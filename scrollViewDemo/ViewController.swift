9049b6d9478662df9654f28ff0b4b3c6b41342ef
kalamkarpooja <122428473+kalamkarpooja@users.noreply.github.com>
2023-06-07 17:51:23 +0530

    Add files via upload

---
+++ scrollViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scrollViewDemo
+//
+//  Created by Mac on 07/06/35.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
